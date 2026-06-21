68d5e874547b12a5d5feed02b977917fe171542d
kwaso2x <kwaziosei@gmail.com>
2026-06-20 21:57:01 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int counter = 0;
+        int max = 0;
+        
+        for(int i= 0; i< nums.length; i++){
+            if(nums[i]==1){
+                counter++;
+            } else {
+                counter = 0;
+            }
+            if(counter > max){
+                max = counter;
+            }
+        }
+        return max;
+    }
+}
